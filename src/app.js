191cf8b5c560d8f1df9f7b5a5e77b0fa0fecb3be
gauravroy04 <roygaurav2004@kgpian.iitkgp.ac.in>
2024-03-18 21:41:37 +0530

    user video model

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
-const app = exppress();
+const app = express();
 
 app.use(cors({
     origin:process.env.CORS_ORIGIN, credentials: true
--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,8 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () =>{
     try {
-        const connectionInstance = mongoose.connect(`${process.env.MONGO_URI}/{DB_NAME}`);
-        console.log("Conncected at host: ",(await connectionInstance).connection.host);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        console.log("Conncected at host: ",connectionInstance.connection.host);
     } catch (error) {
         console.error("Errr: ", error);
         throw error;
