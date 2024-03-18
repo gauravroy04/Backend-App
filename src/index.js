2dc5975db47f6ae153d0ad6570c84aa5325f7ad5
gauravroy04 <roygaurav2004@kgpian.iitkgp.ac.in>
2024-03-18 19:16:50 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import connectDB from "./db/index.js";
 import { DB_NAME } from "./constants.js";
 import {config} from "dotenv";
 import express from 'express';
+import { app } from "./app.js";
 
 config({path:'config.env'});
 console.log(process.env.PORT);
-connectDB();
-console.log("Working");
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+}).catch((err) =>{
+    console.log("Connnection falied : ",err);
+})
